628564c0e0c880a91e7300b81649c50d4c88d006
Saidul Arefin <114782285+Saidul-Arefin@users.noreply.github.com>
2023-08-03 01:26:44 +0600

    even sum and odd sum

--- arraysum.js
+++ arraysum.js
@@ -1,14 +1,18 @@
 function getSum(array) {
   let sum = 0;
-  let arr1 = 0;
+  let evenSum = 0;
+  let oddSum =0
   for (let i = 0; i < array.length; i++) {
     const element = array[i];
     sum += element;
     if ( element % 2 ===0)
-    {arr1 += element}
+    {evenSum += element}
+    else{
+        oddSum += element
+    }
 
   }
-  return [`${sum} is array sum`, `${arr1} is odd sum`]
+  return [`${sum} is array sum`, `${evenSum} is even sum` , `${oddSum} is odd sum`]
 }
 
 const numbers = [12, 65, 45, 78, 32, 45, 91];
